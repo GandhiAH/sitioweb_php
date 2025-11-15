e611e9830e7820e99b9fded52fe46512449481ac
GandhiAH <gandhi.agusxd@gmail.com>
2025-11-15 17:23:21 -0500

    Create database.php

---
+++ database.php
@@ -0,0 +1,14 @@
+<?php
+    $host = '127.0.0.1';
+    $port = '4306';
+    $username = 'root';
+    $password = '';
+    $database = 'sitiowebphpp';
+
+    try {
+        $conn = new PDO("mysql:host=$host;port=$port;dbname=$database;charset=utf8", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+    die("Connection Failed: " . $e->getMessage());
+    }
+?>
